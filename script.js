527e30873e89856a7f0e86ac982b010fa8494ec7
collins-ai <collinsosas843@gmail.com>
2024-11-01 18:06:50 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*fetch(`./data.json`).then(request => {
+    return request.json()
+}).then(data => {
+    const displayList = (dataValue) => {
+        const myList = document.getElementById(`list`)
+        myList.innerHTML += `<li>
+        <span>${dataValue.title}</span>
+        </li>`
+    }
+
+    for(const dataValue of data) {
+        displayList(dataValue)
+    }
+})*/
+
+
+
+
