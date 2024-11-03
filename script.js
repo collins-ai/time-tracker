7f77f7cc30e7c0031504f79397f1e7780b5488c0
collins-ai <collinsosas843@gmail.com>
2024-11-03 09:33:17 -0800

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-/*fetch(`./data.json`).then(request => {
+fetch(`./data.json`).then(request => {
     return request.json()
 }).then(data => {
-    const displayList = (dataValue) => {
-        const myList = document.getElementById(`list`)
-        myList.innerHTML += `<li>
-        <span>${dataValue.title}</span>
-        </li>`
-    }
+    const totalTitle = document.getElementsByClassName(`title`)
+    const totalCurrent = document.getElementsByClassName(`current`)
+    const totalPrevious = document.getElementsByClassName(`previous`)
+    const daily = document.getElementById(`day`)
+    const weekly = document.getElementById(`week`)
+    const monthly = document.getElementById(`month`)
 
-    for(const dataValue of data) {
-        displayList(dataValue)
-    }
-})*/
+    data.forEach((obj, index) => {
+        totalTitle[index].innerHTML = obj.title
+        totalCurrent[index].innerHTML = `${obj.timeframes.daily.current}hrs`
+        totalPrevious[index].innerHTML = `Last Week - ${obj.timeframes.daily.previous}hrs`
+    })
 
+    daily.addEventListener(`click`, () => {
+        data.forEach((obj, index) => {
+            totalCurrent[index].innerHTML = `${obj.timeframes.daily.current}hrs`
+            totalPrevious[index].innerHTML = `Last Week - ${obj.timeframes.daily.previous}hrs`
+        })
+    })
 
+    weekly.addEventListener(`click`, () => {
+        data.forEach((obj, index) => {
+            totalCurrent[index].innerHTML = `${obj.timeframes.weekly.current}hrs`
+            totalPrevious[index].innerHTML = `Last Week - ${obj.timeframes.weekly.previous}hrs`
+        })
+    })
 
+    monthly.addEventListener(`click`, () => {
+        data.forEach((obj, index) => {
+            totalCurrent[index].innerHTML = `${obj.timeframes.monthly.current}hrs`
+            totalPrevious[index].innerHTML = `Last Week - ${obj.timeframes.monthly.previous}hrs`
+        })
+    })
+})
 
